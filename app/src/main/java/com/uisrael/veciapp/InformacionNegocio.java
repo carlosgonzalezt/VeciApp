1e410167cf16a22efbcae6063452aa73691b47ef
Charlie <c.gonzalez.t.91@gmail.com>
2019-05-24 21:41:10 -0500

    Charlie cambios

--- app/src/main/java/com/uisrael/veciapp/InformacionNegocio.java
+++ app/src/main/java/com/uisrael/veciapp/InformacionNegocio.java
@@ -4,13 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.MapView;
 
 public class InformacionNegocio extends AppCompatActivity {
+
+    private TextView ob_tv_titulo2;
+    private Spinner ob_sp_negocio;
+    private EditText ob_pt_nombrenegocio;
+    private MapView ob_mapView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion_negocio);
+
+        ob_tv_titulo2 = (TextView) findViewById(R.id.tv_titulo2);
+        ob_sp_negocio = (Spinner) findViewById(R.id.sp_negocio);
+        ob_pt_nombrenegocio = (EditText) findViewById(R.id.pt_nombrenegocio);
+        ob_mapView1 = (MapView) findViewById(R.id.mapView1);
+
+        //Asignar los valores al spiner para seleccionar
+        String[] opciones = {"Tienda", "Verduleria / Fruteria o similares", "Carniceria", "Papeleria", "Centro de computo", "Ferreteria", "Farmacia", "Costureria", "Otros"};
+        ArrayAdapter<String> objeto_adapter = new ArrayAdapter<String>(this, R.layout.sppiner, opciones);
+        ob_sp_negocio.setAdapter(objeto_adapter);
     }
 
     public void regresar(View view){
--- app/src/main/java/com/uisrael/veciapp/MainActivity.java
+++ app/src/main/java/com/uisrael/veciapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.security.PrivateKey;
 
 public class MainActivity extends AppCompatActivity {
     //Universidad Israel
@@ -12,10 +15,28 @@ public class MainActivity extends AppCompatActivity {
     //Jairo Pulgarin
     //9no C Sistemas
 
+    //Variables d los objetos en el activity
+    private EditText ob_pt_nombre;
+    private EditText ob_pt_apellido;
+    private EditText ob_pt_correo;
+    private EditText ob_pt_usuario;
+    private EditText ob_pt_clave;
+    private EditText ob_pt_repclave;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se asignan las variables a los objetos creados en el activity
+        ob_pt_nombre = (EditText) findViewById(R.id.pt_nombre);
+        ob_pt_apellido = (EditText) findViewById(R.id.pt_apellido);
+        ob_pt_correo = (EditText) findViewById(R.id.pt_correo);
+        ob_pt_usuario = (EditText) findViewById(R.id.pt_usuario);
+        ob_pt_clave = (EditText) findViewById(R.id.pw_clave);
+        ob_pt_repclave = (EditText) findViewById(R.id.pw_repclave);
+
     }
 
     public void siguiente(View view){
