4e877f8d08e35e6cad14f5e0f75fd5040da478c4
Charlie <c.gonzalez.t.91@gmail.com>
2019-06-02 23:48:59 -0500

    Avance 20190602

---
+++ app/src/main/java/com/uisrael/veciapp/Loading.java
@@ -0,0 +1,20 @@
+package com.uisrael.veciapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+
+    public void cargar(View view){
+        Intent sig = new Intent(this,MainActivity.class);
+        startActivity(sig);
+    }
+}
