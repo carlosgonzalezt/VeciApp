ac90870118cd70b217cfc9d280759eba907bce76
Jairo Pulgarin <40905226+Jairop1904@users.noreply.github.com>
2019-07-25 00:30:52 -0500

    jairo

--- app/src/main/java/com/uisrael/veciapp/navegador.java
+++ app/src/main/java/com/uisrael/veciapp/navegador.java
@@ -1,6 +1,7 @@
 package com.uisrael.veciapp;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
