107a2ebf67e367d465900e91f6d56b2fbdf6b461
Charlie <c.gonzalez.t.91@gmail.com>
2019-07-30 13:40:26 -0500

    Avance 20190730 1340

---
+++ app/src/main/java/com/uisrael/veciapp/WVNavegador.java
@@ -0,0 +1,31 @@
+package com.uisrael.veciapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WVNavegador extends AppCompatActivity {
+
+    WebView buscador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wvnavegador);
+
+        // Definimos el webView
+        buscador=(WebView)findViewById(R.id.wv_navegador);
+
+        buscador.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        // Cargamos la web
+        buscador.loadUrl("http://www.movilzona.es");
+    }
+}
