a4a645a721c6cfa552aec1606939849cd3e3edfe
Charlie <c.gonzalez.t.91@gmail.com>
2019-07-31 22:09:15 -0500

    Avance 20190731 2209

--- app/src/main/java/com/uisrael/veciapp/PruebaMapa.java
+++ app/src/main/java/com/uisrael/veciapp/PruebaMapa.java
@@ -15,7 +15,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class PruebaMapa extends AppCompatActivity {
     private MapView mapView;
     private GoogleMap gmap;
-    private Toolbar tb;
+    //private Toolbar tb;
 
     private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
@@ -24,9 +24,9 @@ public class PruebaMapa extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba_mapa);
 
-        tb = findViewById(R.id.toolbar);
+        //tb = findViewById(R.id.toolbar);
         //setSupportActionBar(tb);
-        tb.setSubtitle("MapView");
+        //tb.setSubtitle("MapView");
 
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
